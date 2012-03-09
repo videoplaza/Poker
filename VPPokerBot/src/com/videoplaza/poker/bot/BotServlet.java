46a01f836a436ccdcfe2fd03bbe512b5299aa1e9
joachim <joachim@videoplaza.com>
2012-03-09 11:31:40 +0100

    removed VPPokerBot from gitignore

--- VPPokerBot/src/com/videoplaza/poker/bot/BotServlet.java
+++ VPPokerBot/src/com/videoplaza/poker/bot/BotServlet.java
@@ -21,7 +21,7 @@ public class BotServlet extends HttpServlet {
    private static final long serialVersionUID = 3277126036475556350L;
    private URLCodec encoder = new URLCodec();
 
-   private Bot botImpl = new Jakob();
+   private Bot botImpl = new ExampleBotImpl();
 
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
